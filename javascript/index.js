a0cffceceb453ede72cdcb1085839b2f297bc1fd
Eiks-exe <66579704+Eiks-exe@users.noreply.github.com>
2021-01-10 00:11:37 -0400

    Change color

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,20 @@
+window.addEventListener("load", startup, false);
+
+function startup(){
+  var colorpicker = document.querySelector("#colorpicker")
+  colorpicker.value = '#dd00ff' ;
+  colorpicker.addEventListener("input",update,false)
+  colorpicker.select()
+
+}
+function update(e) {
+  var embedcolor = document.querySelector('#embedcolor');
+  
+  if (embedcolor) {
+    embedcolor.style.background = e.target.value;
+  }
+}
+
 async function sendMessage() {
     
     var request = new XMLHttpRequest();
@@ -6,13 +23,15 @@ async function sendMessage() {
     const Content = document.getElementById('Content').value || "" ;
     const Messagelinks = "" 
     const Thumbnail = document.getElementById('Thumbnail').value || ""; 
-    const Image = document.getElementById('Image').value || ""
+    const Image = document.getElementById('Image').value || "";
+    var color = document.querySelector('#colorpicker').value || "";
     request.open("POST", WhookUrl)
     request.setRequestHeader('Content-type', 'application/json');
     
     var Embed = {
       title : Title,
       description : Content,
+      color : hexToDecimal(color) ,
       thumbnail : 
       {
         url : Thumbnail
