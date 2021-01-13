083a5d8575b9c1d53e67d39d28c2f3cb7e1870bf
Eiks-exe <66579704+Eiks-exe@users.noreply.github.com>
2021-01-13 16:37:38 -0400

    everyone

--- javascript/index.js
+++ javascript/index.js
@@ -3,16 +3,31 @@ window.addEventListener("load", startup, false);
 function startup(){
   var colorpicker = document.querySelector("#colorpicker")
   var avatar = document.querySelector("img")
+  var name = document.querySelector('#Whooktitle')
+  name.innerHTML = 'send your embed ^^'
   avatar.src = "https://cdn.discordapp.com/attachments/759679730987040811/797176880394010695/unknown.png"
   colorpicker.value = '#4287f5' ;
   colorpicker.addEventListener("input",update,false)
   colorpicker.select()
+  var h  = document.querySelector('.h')
+  var i  = document.querySelector('.i')
+  h.style.visibility = 'hidden';
+  i.style.visibility = 'hidden';
   var WebHook = document.querySelector("#WebHook")
   WebHook.addEventListener("change",avatarChange, false)
+  const Title = document.getElementById('Title')
+  Title.addEventListener("change",update, false)
+
 }
 function update(e) {
   var embedcolor = document.querySelector('#embedcolor');
-  
+  const Title = document.getElementsByClassName('Title')
+  var i  = document.querySelector('.i')
+  if(Title){
+    i.style.visibility = 'visible';
+  }else{
+    startup
+  }
   if (embedcolor) {
     embedcolor.style.background = e.target.value;
   }
@@ -27,13 +42,20 @@ async function avatarChange(e){
           var avatar = out.avatar
           var img = document.querySelector('img')
           var name = document.querySelector('#Whooktitle')
-          
+          var h  = document.querySelector('.h')
           if(id && avatar){
             img.src = `https://cdn.discordapp.com/avatars/${id}/${avatar}`
             name.innerHTML = out.name
+            
+            h.style.visibility = 'visible'
           }
-      }).catch(err => console.error(err)); 
+      }).catch(err => {
+        console.error(err)
+        startup();
+      }); 
 }
+
+
 async function sendMessage() {
     
     var request = new XMLHttpRequest();
@@ -44,27 +66,53 @@ async function sendMessage() {
     const Thumbnail = document.getElementById('Thumbnail').value || ""; 
     const Image = document.getElementById('Image').value || "";
     var color = document.querySelector('#colorpicker').value || "";
+    var check = document.querySelector('#everyone');
+    
+    
     request.open("POST", WhookUrl)
     request.setRequestHeader('Content-type', 'application/json');
-    
-    var Embed = {
-      title : Title,
-      description : Content,
-      color : hexToDecimal(color) ,
-      thumbnail : 
-      {
-        url : Thumbnail
-      },
-      image : 
-      {
-        url : Image
-      },
-    }
-    var params = {
-      username: "",
-      embeds : [ Embed ]
+    if(check.checked == true){
+      var Embed = {
+        title : Title,
+        description : Content,
+        color : hexToDecimal(color) ,
+        thumbnail : 
+        {
+          url : Thumbnail
+        },
+        image : 
+        {
+          url : Image
+        },
+      }
+      var params = {
+        username: "",
+        embeds : [ Embed ],
+        content: '@everyone'
+      }
+      
+    }else {
+      var Embed = {
+        title : Title,
+        description : Content,
+        color : hexToDecimal(color) ,
+        thumbnail : 
+        {
+          url : Thumbnail
+        },
+        image : 
+        {
+          url : Image
+        },
+      }
+      var params = {
+        username: "",
+        embeds : [ Embed ],
+        content: ''
+      }
       
     }
+ 
     
     request.send(JSON.stringify(params));
     function hexToDecimal(hex) {
